11c2f99a766dd05ffea8018cdc091ea285b1090f
Shane Gable <gableshane@gmail.com>
2019-12-10 16:43:36 -0800

    small refactor

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 'use strict';
-// GLOBAL VARIABLES AND DEPENDENCIES
-let weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
-let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-let error = {
-  status: 500,
-  responseText: "Sorry, something went wrong",
-}
+
+// DEPENDENCIES
 const PORT = process.env.PORT || 3000;
 const express = require('express');
 const cors = require('cors');
@@ -13,10 +8,19 @@ const app = express();
 require('dotenv').config();
 app.use(cors());
 
+// GLOBAL VARIABLES
+let weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'];
+let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+let error = {
+  status: 500,
+  responseText: "Sorry, something went wrong",
+}
+
 // LOCATION PATH
 app.get('/location', (request, response) => {
+
   const geoData = require('./data/geo.json');
-  let query = request.query['data'].toLowerCase();
+  let query = request.query.data.toLowerCase();
   const location = geoData.results[0].geometry.location;
   const formAddr = geoData.results[0].formatted_address;
   const search_query = geoData.results[0].address_components[0].short_name.toLowerCase();
@@ -29,11 +33,13 @@ app.get('/location', (request, response) => {
 });
 // LOCATION CONSTRUCTOR FUNCTION
 function Geolocation (search_query,formAddr,location) {
+
   this.search_query = search_query;
   this.formatted_address = formAddr;
-  this.latitude = location['lat'];
-  this.longitude = location['lng'];
+  this.latitude = location.lat;
+  this.longitude = location.lng;
 }
+
 // WEATHER PATH
 app.get('/weather', (request , response ) => {
   const reply = [];
@@ -44,11 +50,13 @@ app.get('/weather', (request , response ) => {
   }
   response.send( reply );
 })
+
 // FORECAST CONSTRUCTOR FUNCTION
 function Forecast (summary, time) {
   this.forecast = summary;
   this.time = getDate(new Date(time));
 }
+
 // RETURNS FORMATTED DATE STRING
 function getDate (time) {
   let day = weekday[time.getDay()];
